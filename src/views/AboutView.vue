ef0ad1ca683735401303caaecda46f278f357a27
Kingsley <kifeanyi216@gmail.com>
2025-10-28 19:41:36 +0100

    HomeView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>About Page</div>
+  <div>
+    <TwoInOne
+      :name="'Welcome To About Page'"
+      :imgsrc="home"
+      :visit="'/services'"
+      :btnName="'Get Started'"
+    />
+  </div>
 </template>
 
 <script setup>
+import TwoInOne from "../components/TwoInOne";
+import home from "../assets/images/home.png";
 </script>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>Home Page</div>
+  <div>
+    <TwoInOne
+      :name="'Grow Your Business With'"
+      :imgsrc="home"
+      :visit="'/about'"
+      :btnName="'Get Started'"
+    />
+  </div>
 </template>
 
 <script setup>
+import TwoInOne from "../components/TwoInOne";
+import home from "../assets/images/home.png";
 </script>
 
 <style scoped>
